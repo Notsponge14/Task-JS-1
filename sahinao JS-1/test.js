0d4419609369e2f937ed227c0ba65ae19a7a6d77
Notsponge <arevadzezu2009@gmail.com>
2024-12-14 18:18:13 +0200

    Add files via upload

---
+++ sahinao JS-1/test.js
@@ -0,0 +1,183 @@
+// პირველი მაგალითი
+
+// let zuka = 10;
+// zuka = 15;
+// zuka = 7;
+// console.log(zuka);
+
+// let saba = 37;
+// saba = 140;
+// saba = 220;
+// console.log(saba);
+
+
+//მეორე მაგალითი
+
+// const zuka = 35;
+// console.log(zuka);
+
+
+// const gela = 140;
+// console.log(gela);
+
+
+//მესამე მაგალითი
+
+// let x = 40;
+// let y = 70;
+// console.log(x + y);
+
+
+
+// let x = 300;
+// let y = 1000;
+// console.log(y - x);
+
+
+
+// let x = 10;
+// let y = 5;
+// console.log(x * y);
+
+
+
+// let x = 15;
+// let y = 3;
+// console.log(x / y);
+
+
+// let x = 20;
+// let y = 5;
+// let z = 2;
+// console.log(x + y * z);
+
+
+
+// let x = 10;
+// let y = 20;
+// let z = 50;
+// let p = 30;
+// let t = 100;
+// const sum = t / y + p * x - z ;
+// console.log(t / y + p * x - z);
+
+
+
+
+//მეოთხე მაგალითი
+
+
+
+// console.log("Hello World")
+
+// let x = 300;
+
+// console.log(x / (30 + 20));
+
+
+
+
+//მეხუთე მაგალითი
+
+
+
+// let x = 10;
+// x++
+// x++
+// console.log(x);
+
+
+
+// let x = 8;
+// x++
+// x++
+// console.log(x);
+
+
+// let x = 7;
+// x--;
+// x--;
+// console.log(x);
+
+
+
+// let y = 10;
+// y--;
+// y--;
+// y--;
+// console.log(y)
+
+
+//მეექვსე მაგალითი
+
+
+// let x = 5;
+// x += 4;
+// console.log(x)
+
+
+// let x= 10;
+// x += 15;
+// console.log(x)
+
+
+
+// let x = 10;
+// x -= 2;
+// console.log(x)
+
+
+// let x = 15;
+// x -= 5;
+// x -= 2;
+// console.log(x);
+
+
+
+// let x = 25;
+// x *= 2;
+// console.log(x)
+
+
+
+// let x = 15;
+// x *= 3;
+// x *= 2;
+// console.log(x)
+
+
+// let x = 30;
+// x /= 10;
+// console.log(x)
+
+
+// let x = 100;
+// x /= 10;
+// x /= 2;
+// console.log(x)
+
+
+
+
+//მეშვიდე მაგალითი
+
+
+// let x = 30;
+// let y = 10;
+// console.log(x<y)
+
+
+// let x = 30;
+// let y = 50;
+// console.log(x<y)
+
+
+// let x = 30;
+// let y = 20;
+// console.log(x>y)
+
+
+
+// let x = 20;
+// let y = 40;
+// console.log(x>y)
